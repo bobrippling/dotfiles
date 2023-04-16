121cba1883020dc7039395028532cdc762803ee4
Rob Pilling <robpilling@gmail.com>
2023-04-16 14:33:41 +0100

    .vim/.../health.lua: s/report_(ok|info)/\1/

--- .vim/lua/grep/health.lua
+++ .vim/lua/grep/health.lua
@@ -5,7 +5,7 @@ local M = {}
 
 local function maybe_with(exe)
 	if vim.call("executable", exe) then
-		vim.health.report_ok(("Using `%s`"):format(exe))
+		vim.health.ok(("Using `%s`"):format(exe))
 		return true
 	end
 	return false
@@ -14,7 +14,7 @@ end
 M.check = function()
 	vim.health.report_start("grep")
 
-	vim.health.report_info("current global grep: `" .. vim.opt.grepprg:get() .. "`")
+	vim.health.info("current global grep: `" .. vim.opt.grepprg:get() .. "`")
 
 	if not maybe_with("rg") and not maybe_with("ag") then
 		vim.health.report_warn("No search executable found, using grep", "suggestion: install ripgrep or ag")
