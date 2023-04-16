87b75455a2cc3b5fc9eb45a626f90e78d6e35b1d
Rob Pilling <robpilling@gmail.com>
2023-04-16 14:20:08 +0100

    zshcompl-generate.sh: automatic

--- zshcompl-generate.sh
+++ zshcompl-generate.sh
@@ -1,22 +1,81 @@
 #!/bin/sh
 
-if test $# -ne 0
-then
-	echo >&2 "Usage: $0"
+set -e
+
+usage(){
+	echo >&2 "Usage: $0 --all"
+	echo >&2 "       $0 [rust] [pass=src] [z] [fd]"
 	exit 2
-fi
+}
 
 here=~/.config/dotfiles
 
-for cmd in cargo rustup
-do rustup completions zsh "$cmd" >"$here"/zshcompl/_"$cmd"
+maybe_new(){
+	if ! test -e "$2"
+	then echo "new: $1"
+	fi
+}
+
+gen_rust(){
+	for cmd in cargo rustup
+	do
+		dest="$here"/zshcompl/_"$cmd"
+		maybe_new "$cmd" "$dest"
+		rustup completions zsh "$cmd" >"$dest"
+	done
+}
+
+gen_pass(){
+	dest="$here"/zshcompl/_pass
+	maybe_new pass "$dest"
+	ln -fs "$1"/src/completion/pass.zsh-completion "$dest"
+}
+
+gen_z(){
+	mkdir -p contrib
+	(
+		cd contrib
+		if test -e z
+		then
+			cd z
+			git fetch -p --quiet
+			git merge --ff-only --quiet
+		else
+			maybe_new z z
+			git clone --quiet https://github.com/rupa/z
+		fi
+	)
+}
+
+gen_fd(){
+	dest="$here"/zshcompl/_fd
+	maybe_new fd "$dest"
+	curl https://raw.githubusercontent.com/sharkdp/fd/master/contrib/completion/_fd \
+		-fsSL \
+		-o "$dest"
+}
+
+if test $# -eq 0
+then usage
+fi
+
+for arg
+do
+	case "$arg" in
+		--all)
+			gen_rust
+			gen_z
+			gen_fd
+			if ! test -e "$here"/zshcompl/_pass
+			then echo >&2 "can't generate pass: no src"
+			fi
+			;;
+		rust) gen_rust ;;
+		pass=*)
+			gen_pass "${arg#*=}"
+			;;
+		z) gen_z ;;
+		fd) gen_fd ;;
+		*) usage ;;
+	esac
 done
-
-#ln -s <password-store>/src/completion/pass.zsh-completion "$here"/zshcompl/_pass
-
-#mkdir contrib
-#cd contrib && git clone https://github.com/rupa/z
-
-curl https://raw.githubusercontent.com/sharkdp/fd/master/contrib/completion/_fd \
-	-fsSL \
-	-o "$here"/zshcompl/_fd
