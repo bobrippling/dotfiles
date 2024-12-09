74c141905341c051035329cc85102de0c22068f9
Rob Pilling <robpilling@gmail.com>
2024-12-09 18:18:23 +0000

    .vim/.../grep/health.lua: s/report_//

--- .vim/lua/grep/health.lua
+++ .vim/lua/grep/health.lua
@@ -12,12 +12,12 @@ local function maybe_with(exe)
 end
 
 M.check = function()
-	vim.health.report_start("grep")
+	vim.health.start("grep")
 
 	vim.health.info("current global grep: `" .. vim.opt.grepprg:get() .. "`")
 
 	if not maybe_with("rg") and not maybe_with("ag") then
-		vim.health.report_warn("No search executable found, using grep", "suggestion: install ripgrep or ag")
+		vim.health.warn("No search executable found, using grep", "suggestion: install ripgrep or ag")
 	end
 end
 
