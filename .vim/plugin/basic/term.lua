f02f42ff31d81ab840f57216d3b77c946fcadadf
Rob Pilling <robpilling@gmail.com>
2025-08-22 17:26:57 +0100

    .vim/.../term.lua: add (sync term pwd with window)

---
+++ .vim/plugin/basic/term.lua
@@ -0,0 +1,33 @@
+vim.api.nvim_create_augroup('luaterm', { clear = true })
+
+local function err(msg, bufnr)
+  vim.notify('buf' .. bufnr .. ': ' .. msg)
+end
+
+vim.api.nvim_create_autocmd({ 'TermRequest' }, {
+  group = 'luaterm',
+  desc = 'Handles OSC 7 dir change requests',
+  callback = function(ev)
+    if string.sub(ev.data.sequence, 1, 4) ~= '\x1b]7;' then
+      return
+    end
+
+    local bufnr = vim.api.nvim_get_current_buf()
+
+    local host, dir = ev.data.sequence:match('\x1b]7;file://([^/]*)(.*)')
+    if host:len() > 0 and host ~= vim.loop.os_gethostname() then
+      err('chdir on host: ' .. host .. ' (ignoring)')
+      return
+    end
+
+    if vim.fn.isdirectory(dir) == 0 then
+      err('invalid dir: ' .. dir)
+      return
+    end
+
+    vim.api.nvim_buf_set_var(ev.buf, 'osc7_dir', dir)
+    if ev.buf == bufnr then
+      vim.cmd.lcd(dir)
+    end
+  end
+})
